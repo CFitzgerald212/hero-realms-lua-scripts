088fe5ba9c1065c184f5e8b5717287dc132e03d0
Cooper Fitzgerald <cfitzgerald212@gmail.com>
2025-02-18 23:30:13 +1100

    initial commit

---
+++ Filtro/visualElements.lua
@@ -0,0 +1,18 @@
+axeReworkXml= [[
+<vlayout>
+    <hlayout flexibleheight="3">
+        <tmpro text="{combat_3}" fontsize="60"/>
+    </hlayout>
+    <hlayout flexibleheight="3">
+        <tmpro text="Draw a card if a guard is in play" fontsize="20" flexiblewidth="1" />
+    </hlayout>
+</vlayout>
+]]
+
+flagRework = [[
+<vlayout>
+    <hlayout>
+           <tmpro text="{gold_2} {combat_1}" fontsize="60" flexiblewidth="6"/>
+    </hlayout>
+</vlayout>
+]]
